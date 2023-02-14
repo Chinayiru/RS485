f9644e3ef96ceb24d421fe2f95936da32e3cfb3d
yiru <yi_ru@qq.com>
2023-02-14 11:48:53 +0800

    第四版本

---
+++ src/main/java/com/lt/util/ParityForBytes.java
@@ -0,0 +1,7 @@
+package com.lt.util;/**
+ *
+ * @aythor yi
+ * @data 2023/2/14 itime
+ * @Description
+*/public class setParityForBytes {
+}
---
+++ src/main/java/com/lt/util/init1.java
@@ -0,0 +1,9 @@
+package com.lt.util;
+
+/**
+ * @aythor yi
+ * @data 2023/2/13 itime
+ * @Description
+ */
+public class init {
+}
